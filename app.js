f97bd70ce1fe58eb63270e83363d02d5675d9fc2
mkiba <martial.kiba@gmail.com>
2024-06-12 17:29:42 +0000

    v1

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
 const fs = require("fs");
 const prompt = require('prompt-sync')({sigint: true});;
 
-const files_list_filename = "files_written.txt";
+const files_dir = "./files";
+const files_list_filename = files_dir+"/files_written.txt";
 
-var file_list = [];
+let file_list = [];
 
 //console.log(process.argv);
 
-myArgs = process.argv
-
-const testWriteFileExists = () => {
-    if (!fs.existsSync(`./files/${files_list_filename}`)) {
-        console.log(`creating ./files/${files_list_filename}`);
-        var createStream = fs.createWriteStream(`./files/${files_list_filename}`);
-        createStream.end();
-    }
-}
+myArgs = process.argv;
 
 const loadFileNames = () => {
-    fs.readFile(`./files/${files_list_filename}`, (err, result) => {
-        if (err) {
-            throw err;
-        } else {
-            console.log(result);
-            result = result.toString().toUpperCase();
-            file_list = result.split("\n");
-        }
-    })
+    if (!fs.existsSync(files_list_filename)) {
+        console.log("File does not exist. Creating it ...")
+        const fd = fs.openSync(files_list_filename, 'w', 0o666);
+        fs.closeSync(fd);
+    }
+    let data = fs.readFileSync(files_list_filename, { encoding: 'utf8', flag: 'r' });
+    if (data) {
+        //console.log("Read result:", data);
+        data = data.toString().toUpperCase();
+        file_list = data.split("\n");
+    }
 }
 
 if (myArgs.length <= 2) {
     console.log("You must provide a filename!");
 } else {
-    testWriteFileExists();
     loadFileNames();
-    var fileName = myArgs[2];
+    let fileName = myArgs[2];
+
     if (file_list.includes(fileName.toUpperCase())) {
-        const name = prompt('Enter new file name: ');
-        console.log(`File name is ${name}`);
+        const name = prompt('file name exists Enter new file name: ');
+        
         if (fileName !== name) {
             fileName = name;        
         } else {
@@ -45,19 +40,22 @@ if (myArgs.length <= 2) {
         }
     }
     if (fileName.length > 0) {
-        fs.appendFile(`./files/${fileName}`, "You are awesome\n", (err) => {
+        fs.appendFile(`${files_dir}/${fileName}`, "You are awesome\n", (err) => {
             if (err) {
                 throw err;
             } else {
                 fileName = fileName.toUpperCase()
                 file_list.push(fileName);
-                fs.appendFile(`./files/${files_list_filename}`, fileName, (err) => {
+                fs.appendFile(files_list_filename, fileName+"\n", (err) => {
                     if (err) {
                         console.log("unable to append writted file name.");
+                    } else {
+                        console.log("Data written successfully!");
                     }
                 })
-                console.log("Data written successfully!");
             }
         })    
+    } else {
+        console.log("File name not provided. Exiting ...");
     }
 }
